63de76a3e67bc3bd3892be484e00d5d547d0ed10
Wenceslas HOTOEUA <wenceslas.hotoeua@gmail.com>
2024-02-15 17:05:16 -0500

    1er commit: Debut du code, ajout des consignes

---
+++ Composant.java
@@ -0,0 +1,35 @@
+public class Composant {
+    private final String categorie;
+    private String marque;
+    private String nom;
+    private double prix;
+    private double rabais;
+
+    public Composant(String categorie, String marque, String nom, double prix){
+        this.categorie = categorie.toUpperCase();
+        this.marque = marque;
+        this.nom = nom;
+        this.prix = prix > 0 ? prix : 0;
+        this.rabais = 0;
+    }
+
+    public Composant Copier(){
+        //Faire methode de copie standard de l'objet
+
+    }
+
+    public boolean estIdentique(){
+        //Faire methode pour vérifier si deux composants sont égaux sur la base de la catégorie, de la
+        //marque et du nom
+    }
+
+    public double getPrix(){
+        //Faire accesseur qui devrai retourner le prix AVEC le rabais
+    }
+
+    public String toString(){
+        //Faire methode pour convertir un composant en String, comme ceci:
+        // [categorie] marque,nom
+    }
+
+}
---
+++ Configuration.java
@@ -0,0 +1,7 @@
+public class Configuration {
+    private final String description;
+    private final double prixMax;
+    private Composant[] composants;
+    private int nombreComposants;
+    private static  final int MAX_COMPOSANTS = 20;
+}
