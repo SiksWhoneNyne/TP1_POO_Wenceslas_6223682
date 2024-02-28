f2725734608f8e8c889316de048804d3c4a36870
Wenceslas HOTOEUA <wenceslas.hotoeua@gmail.com>
2024-02-27 22:00:46 -0500

    4eme commit: programme fonctionnel dans son ensemble mais probleme lors de certains tests

--- Composant.java
+++ Composant.java
@@ -1,48 +1,60 @@
 public class Composant {
     private final String categorie;
-    private String marque;
-    private String nom;
+    private final String marque;
+    private final String nom;
     private double prix;
     private double rabais;
 
-    public Composant(String categorie, String marque, String nom, double prix){
+    public Composant(String categorie, String marque, String nom, double prix) {
         this.categorie = categorie.toUpperCase();
         this.marque = marque;
         this.nom = nom;
-        this.prix = prix > 0 ? prix : 0;
+        this.prix = prix;
         this.rabais = 0;
     }
 
-    public Composant copier(){
-        //Faire methode de copie standard de l'objet
-        Composant copie = new Composant(this.categorie, this.marque, this.nom, this.prix);
-        copie.rabais = this.rabais;
-        return copie;
+    public Composant copier() {
+        return new Composant(categorie, marque, nom, prix);
     }
 
-    public boolean estIdentique(Composant autre){
-        //Faire methode pour vérifier si deux composants sont égaux sur la base de la catégorie, de la
-        //marque et du nom
-        return this.categorie.equals(autre.categorie) &&
-                this.marque.equals(autre.marque) &&
-                this.nom.equals(autre.nom);
+    public boolean estIdentique(Composant autre) {
+        return this.categorie.equals(autre.getCategorie()) &&
+                this.marque.equals(autre.getMarque()) &&
+                this.nom.equals(autre.getNom());
     }
 
-    public double getPrix(){
-        //Faire accesseur qui devrai retourner le prix AVEC le rabais
-        return this.prix * (1 - this.rabais);
-
+    public double getPrix() {
+        return prix * (1 - rabais);
     }
+
+    public String getCategorie() {
+        return categorie;
+    }
+
+    public String getMarque() {
+        return marque;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setPrix(double prix) {
+        this.prix = prix;
+    }
+
+    public double getRabais() {
+        return rabais;
+    }
+
     public void setRabais(double rabais) {
         if (rabais >= 0 && rabais <= 1) {
             this.rabais = rabais;
         }
     }
-    public String toString(){
-        //Faire methode pour convertir un composant en String, comme ceci:
-        // [categorie] marque,nom
-        return String.format("[%s] %s,%s", categorie, marque, nom);
 
+    @Override
+    public String toString() {
+        return "[" + categorie + "] " + marque + " " + nom;
     }
-
 }
--- Configuration.java
+++ Configuration.java
@@ -1,7 +1,102 @@
 public class Configuration {
     private final String description;
     private final double prixMax;
-    private Composant[] composants;
-    private int nombreComposants;
-    private static  final int MAX_COMPOSANTS = 20;
+    private final Composant[] composants;
+    private int nbComposants;
+    private static final int MAX_COMPOSANTS = 20;
+
+    public Configuration(String description, double prixMax, Composant[] composants) {
+        this.description = description.toUpperCase();
+        this.prixMax = prixMax;
+        this.nbComposants = composants.length;
+        this.composants = new Composant[MAX_COMPOSANTS];
+        for (int i = 0; i < nbComposants; i++) {
+            this.composants[i] = composants[i].copier();
+        }
+    }
+
+    public Configuration(Configuration originale) {
+        this.description = originale.description;
+        this.prixMax = originale.prixMax;
+        this.nbComposants = originale.nbComposants;
+        this.composants = new Composant[MAX_COMPOSANTS];
+        for (int i = 0; i < nbComposants; i++) {
+            this.composants[i] = originale.composants[i].copier();
+        }
+    }
+
+    public double calculerTotal(double taxe) {
+        double total = 0;
+        for (int i = 0; i < nbComposants; i++) {
+            total += composants[i].getPrix();
+        }
+        return total * (1 + taxe);
+    }
+
+    public Composant rechercher(String categorie) {
+        for (Composant composant : composants) {
+            if (composant != null && composant.getCategorie().equalsIgnoreCase(categorie)) {
+                return composant;
+            }
+        }
+        return null;
+    }
+
+    public boolean ajouter(Composant composant) {
+        if (nbComposants >= MAX_COMPOSANTS || calculerTotal(0) + composant.getPrix() > prixMax) {
+            return false;
+        }
+        for (int i = 0; i < nbComposants; i++) {
+            if (composants[i].getCategorie().equalsIgnoreCase(composant.getCategorie())) {
+                return false;
+            }
+        }
+        composants[nbComposants++] = composant.copier();
+        return true;
+    }
+
+    public boolean retirer(Composant composant) {
+        for (int i = 0; i < nbComposants; i++) {
+            if (composants[i].estIdentique(composant)) {
+                for (int j = i; j < nbComposants - 1; j++) {
+                    composants[j] = composants[j + 1];
+                }
+                composants[--nbComposants] = null;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean remplacer(Composant composant) {
+        for (int i = 0; i < nbComposants; i++) {
+            if (composants[i].getCategorie().equalsIgnoreCase(composant.getCategorie())) {
+                composants[i] = composant.copier();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String toString() {
+        String result = description + " (max " + prixMax + ",00$) :\n";
+        int index = 1;
+
+        for (Composant composant : composants) {
+            if (composant != null) {
+                result += index + " : " + composant.toString() + "\n";
+                index++;
+            }
+        }
+
+        return result;
+    }
+
+    public int getNbComposants() {
+        return nbComposants;
+    }
+
+    public Composant[] getComposants() {
+        return composants;
+    }
 }
